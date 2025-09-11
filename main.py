88952ceacdaf018914e3512b4ed2857f57fe5b08
altyn02 <altynbaktiyar2002@gmail.com>
2025-09-11 16:04:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ markdown
 """)
 st.write ("Hello, world!")
 
+st.write
+("Здесь была Алтынсара")
+a=st.number_input("dO YOU KNOW HOW COOL I AM? RATE FROM 1 TO 10", step=1)
+b=st.text_input("PLS, WRITE SOMETHING 제발")
+
+for i in range(a):
+  total+=i
+  st.write((total)
